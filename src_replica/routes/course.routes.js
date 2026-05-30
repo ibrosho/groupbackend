ba8bd0271550fb3800e8a05de973bb7ae3387b3e
Kasheefah009 <kasheefahadedokun@gmail.com>
2026-05-30 03:57:27 +0100

    feat: update validators, routes and index.js

--- src_replica/routes/course.routes.js
+++ src_replica/routes/course.routes.js
@@ -1,9 +1,9 @@
 import { Router} from "express";
-import { getCourses } from "../controllers/course.controller.js";
+import { getCourses, createCourse } from "../controllers/course.controller.js";
 
 const mainRouter = Router()
 
-
 mainRouter.get("/", getCourses)
+mainRouter.post("/", createCourse)
 
 export default mainRouter;
