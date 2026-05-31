327a881be8d8a3c6b5917aa8372af58042f55ad2
Akanni btc <ibrosho@gmail.com>
2026-05-31 02:35:00 +0100

    controller and middleware

--- src_replica/routes/course.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router} from "express";
-import { getCourses, createCourse } from "../controllers/course.controller.js";
-
-const mainRouter = Router()
-
-mainRouter.get("/", getCourses)
-mainRouter.post("/", createCourse)
-
-export default mainRouter;
